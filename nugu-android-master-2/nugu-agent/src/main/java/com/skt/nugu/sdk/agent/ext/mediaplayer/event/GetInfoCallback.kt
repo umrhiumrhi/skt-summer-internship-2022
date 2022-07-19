f95e9627f5900cda4fa450bc651a7da0cb71db7a
최인석님/Talent 부 <umrhiumrhi@naver.com>
2022-07-19 14:21:57 +0900

    delete

--- nugu-android-master-2/nugu-agent/src/main/java/com/skt/nugu/sdk/agent/ext/mediaplayer/event/GetInfoCallback.kt
+++
@@ -1,8 +0,0 @@
-package com.skt.nugu.sdk.agent.ext.mediaplayer.event
-
-import com.skt.nugu.sdk.agent.ext.mediaplayer.Song
-
-interface GetInfoCallback {
-    fun onSuccess(song: Song?, issueDate: String?, playTime: String?, playListName: String?)
-    fun onFailure(errorCode: String?)
-}
